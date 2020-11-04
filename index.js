35957514ae5820b0de431e005fb5da103aead88f
Thomas Polvora <thomaspolvorars@hotmail.com>
2020-11-04 17:17:00 -0300

    adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/mensagem',function(req,res){
     res.send("Essa mensagem e automatica!!")
 })
 
+app.get('/sobre',function(req,res){
+    res.send("Esta pagina esta sendo desenvolvida por thomas")
+})
+
 app.listen(3000,function(){
     console.log("Conexao inicializada")
 })
