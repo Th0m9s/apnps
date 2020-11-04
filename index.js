ca72f86c9cfc28c4adb5e0cf4acb34b924e40a30
Thomas Polvora <thomaspolvorars@hotmail.com>
2020-11-04 17:53:37 -0300

    configuraçoes iniciais do projeto

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require(path)
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
     res.send("Ola thomas")
-})
-
-app.get('/mensagem',function(req,res){
-    res.send("Essa mensagem e automatica!!")
-})
-
-app.get('/sobre',function(req,res){
-    res.send("Esta pagina esta sendo desenvolvida por thomas")
-})
-
-app.get('/layout',function(req,res){
-    res.send("Esse é o novo layout!")
 })
 
 app.listen(3000,function(){
