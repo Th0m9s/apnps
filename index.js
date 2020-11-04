0c92b4c57f086bac61bc5b89d953df8dde03d5e0
Thomas Polvora <thomaspolvorars@hotmail.com>
2020-11-04 17:18:54 -0300

    alteraçao na criaçao da rota layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/mensagem',function(req,res){
     res.send("Essa mensagem e automatica!!")
 })
 
+app.get('/layout',function(req,res){
+    res.send("Esse é o novo layout!")
+})
+
 app.listen(3000,function(){
     console.log("Conexao inicializada")
 })
