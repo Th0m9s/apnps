0ffb957b8e7462d0b9303ab87c69a282e127d169
Thomas Polvora <thomaspolvorars@hotmail.com>
2020-11-04 18:15:24 -0300

    Usando ejs nas paginas

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
-    res.send("Ola thomas")
+    res.render('index.ejs',{})
+})
+
+app.get('/usuarios',function(req,res){
+    res.render('usuarios.ejs',{ usuarios:[
+        {nome:'Thomas',email:'thomaspolvorars@hotmail.com'},
+        {nome:'Matheus',email:'matheus@hotmail.com'},
+        {nome:'Jose',email:'jose@hotmail.com'},
+        {nome:'Joao',email:'joao@hotmail.com'}
+    ]})
 })
 
 app.listen(3000,function(){
