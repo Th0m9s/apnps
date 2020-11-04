7190cb4889dd7329af2944c2989d426db6e82950
Thomas Polvora <thomaspolvorars@hotmail.com>
2020-11-04 16:59:15 -0300

    index.js modificado de ola mundo para ola thomas

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+    res.send("Ola thomas")
+})
+
+app.get('/mensagem',function(req,res){
+    res.send("Essa mensagem e automatica !")
+})
+
+app.listen(3000,function(){
+    console.log("Conexao inicializada na porta 3000")
+})
